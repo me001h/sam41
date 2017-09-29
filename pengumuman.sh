9a8f360d991e47edd19b0d86dc9f6121d66dd995
KeningauVPS <31593522+KeningauVPS@users.noreply.github.com>
2017-09-30 02:09:30 +0800

    Delete pengumuman.sh

--- pengumuman.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Script Nobita 95
-# Dilarang Keras Mengambil/mencuplik/mengcopy sebagian atau seluruh script ini.
-# Hak Cipta NobiNobita95 (Dilindungi Undang-Undang nomor 19 Tahun 2002)
-curl Nobi Nobita/index.html
-echo ""
