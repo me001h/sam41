5ed366cd65b6020a5d32149b5b9ffbb1947ca117
KeningauVPS <31593522+KeningauVPS@users.noreply.github.com>
2017-09-30 02:00:33 +0800

    Add files via upload

---
+++ pengumuman.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Script Nobita 95
+# Dilarang Keras Mengambil/mencuplik/mengcopy sebagian atau seluruh script ini.
+# Hak Cipta NobiNobita95 (Dilindungi Undang-Undang nomor 19 Tahun 2002)
+curl Nobi Nobita/index.html
+echo ""
